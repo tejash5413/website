380a7226d564a3025683e49bbd7dc1c1f1957d92
tejash5413 <66580218+tejash5413@users.noreply.github.com>
2023-05-16 15:09:06 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,45 @@
+let navbar = document.querySelector(".navbar");
+let searchBox = document.querySelector(".search-box .bx-search");
+
+searchBox.addEventListener("click", () => {
+    navbar.classList.toggle("showInput");
+    if (navbar.classList.contains("showInput")) {
+        searchBox.classList.replace("bx-search", "bx-x");
+    } else {
+        searchBox.classList.replace("bx-x", "bx-search");
+    }
+});
+
+let navLinks = document.querySelector(".nav-links");
+let menuOpenBtn = document.querySelector(".navbar .bx-menu");
+let menuCloseBtn = document.querySelector(".nav-links .bx-x");
+menuOpenBtn.onclick = function() {
+    navLinks.style.left = "0";
+}
+menuCloseBtn.onclick = function() {
+    navLinks.style.left = "-100%";
+}
+
+
+let htmlcssArrow = document.querySelector(".htmlcss-arrow");
+htmlcssArrow.onclick = function() {
+    navLinks.classList.toggle("show1");
+}
+let moreArrow = document.querySelector(".more-arrow");
+moreArrow.onclick = function() {
+    navLinks.classList.toggle("show2");
+}
+let jsArrow = document.querySelector(".js-arrow");
+jsArrow.onclick = function() {
+    navLinks.classList.toggle("show3");
+}
+
+function switchStyle() {
+    if (document.getElementById('styleSwitch').checked) {
+        document.getElementById('gallery').classList.add("custom");
+        document.getElementById('exampleModal').classList.add("custom");
+    } else {
+        document.getElementById('gallery').classList.remove("custom");
+        document.getElementById('exampleModal').classList.remove("custom");
+    }
+}
